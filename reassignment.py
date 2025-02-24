a0ac4fe3d8403bca771e638626001f8d924b9435
wunhand <davidewcasey@gmail.com>
2025-02-24 02:19:58 -0800

    next add

---
+++ reassignment.py
@@ -0,0 +1,6 @@
+base_exponent = input("Enter a base: ")
+power_exponent = input("Enter an exponent: ")
+base_exponent = int(base_exponent)
+power_exponent = int(power_exponent)
+resultant = base_exponent ** power_exponent
+print("{} to the power of {} = {}." .format(base_exponent, power_exponent, resultant))
